dddef945a2659d8d1bf189ef9555c46d65eb772d
Ilana <ilana@tokenai.io>
2018-02-10 17:37:17 -0800

    Fix merge conflicts

--- contracts/TokenAiNetworkToken.sol
+++ contracts/TokenAiNetworkToken.sol
@@ -9,11 +9,7 @@ contract TokenAiNetworkToken is MiniMeToken, VestedToken {
             _tokenFactory,
             0x0,                        // no parent token
             0,                          // no snapshot block number from parent
-<<<<<<< HEAD
             "TokenAI Netwok Token",     // Token name
-=======
-            "TokenAi Netwok Token",     // Token name
->>>>>>> 70baa688f6bb5cc188305809824141aad3c87b0e
             18,                         // Decimals
             "TAI",                      // Symbol
             true                        // Enable transfers
